4f32265b321a4d7e571142483b70884084236887
VeronikaBogdanovich744 <68291551+VeronikaBogdanovich744@users.noreply.github.com>
2022-03-13 11:07:53 +0300

    Add files via upload

---
+++ FiguresList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiguresLR1
+{
+    class FiguresList
+    {
+        private List<BaseFigure> figItems;
+        public BaseFigure this[int index]
+        {
+            get { return figItems[index]; }
+            set { figItems.Add(value); }
+        }
+        public FiguresList()
+        {
+            figItems = new List<BaseFigure>();
+        }
+
+    }
+}
